5f35533ee2a1c434aeb9e0b5acf38364622529cd
PieroGiovanni <pierogiovanni933@gmail.com>
2020-12-05 02:45:12 -0500

    part I

---
+++ src/resolvers/ProductResolver.ts
@@ -0,0 +1,14 @@
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Product } from "../entity/Product";
+
+@Resolver()
+export class ProductResolver {
+  @Mutation(() => Boolean)
+  async createProduct(
+    @Arg("name") name: string,
+    @Arg("quantity") quantity: number
+  ) {
+    await Product.insert({ name, quantity });
+    return true;
+  }
+}
