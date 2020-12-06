7ec0f3e1d614f5108c9bd5a499763a48c7d47997
PieroGiovanni <pierogiovanni933@gmail.com>
2020-12-06 01:52:01 -0500

    crud finished

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ export const startServer = async () => {
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PingResolver, ProductResolver],
+      validate: false,
     }),
     context: ({ req, res }) => ({ req, res }),
   });
--- src/resolvers/ProductResolver.ts
+++ src/resolvers/ProductResolver.ts
@@ -1,14 +1,70 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Field,
+  InputType,
+  Int,
+} from "type-graphql";
+import { In } from "typeorm";
 import { Product } from "../entity/Product";
+
+@InputType()
+class ProductInput {
+  @Field()
+  name!: string;
+
+  @Field()
+  quantity!: number;
+}
+
+@InputType()
+class ProductUpdateInput {
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field(() => Int, { nullable: true })
+  quantity?: number;
+}
 
 @Resolver()
 export class ProductResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => Product)
   async createProduct(
-    @Arg("name") name: string,
-    @Arg("quantity") quantity: number
+    @Arg("options", () => ProductInput) options: ProductInput
   ) {
-    await Product.insert({ name, quantity });
+    const newProduct = Product.create(options);
+    console.log(newProduct);
+    return await newProduct.save();
+  }
+
+  @Query(() => [Product])
+  async getProducts() {
+    return await Product.find();
+  }
+
+  @Query(() => Product)
+  async getProduct(@Arg("id", () => Int) id: number) {
+    return await Product.findOne(id);
+  }
+
+  @Mutation(() => Boolean)
+  async deleteProduct(@Arg("id", () => Int) id: number) {
+    await Product.delete(id);
     return true;
+  }
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Arg("id", () => Int)
+    id: number,
+
+    @Arg("fields", () => ProductUpdateInput)
+    fields: ProductUpdateInput
+  ) {
+    await Product.update({ id }, fields);
+
+    return await Product.findOne(id);
   }
 }
